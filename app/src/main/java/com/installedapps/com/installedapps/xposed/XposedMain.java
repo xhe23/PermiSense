db3eda6a6cfac017fdba0c762d1704c88755ecdf
Jianfeng Zhang <swordfeng123@gmail.com>
2019-12-04 17:05:29 -0800

    fix sensor native code loader

--- app/src/main/java/com/installedapps/com/installedapps/xposed/XposedMain.java
+++ app/src/main/java/com/installedapps/com/installedapps/xposed/XposedMain.java
@@ -1,30 +1,58 @@
 package com.installedapps.com.installedapps.xposed;
 
 import android.content.pm.ApplicationInfo;
-import android.os.Build;
+import android.util.Log;
 
 import com.installedapps.com.installedapps.BuildConfig;
 
+import java.io.File;
 import java.util.HashMap;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-
-public class XposedMain implements IXposedHookLoadPackage {
+public class XposedMain implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     private HashMap<String, SensorHook> sensorHooks = new HashMap<>();
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName == null || lpparam.packageName.equals("android") ||
                 lpparam.appInfo == null || (lpparam.appInfo.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0 ||
-                lpparam.packageName.startsWith(BuildConfig.APPLICATION_ID)) return;
+                lpparam.packageName.startsWith(BuildConfig.APPLICATION_ID))
+            return;
         hookApplication(lpparam);
     }
 
+    private static String modulePath = null;
+    private static String nativeLibPath = null;
+
     private void hookApplication(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        nativeLibPath = mixLibPath(modulePath, lpparam.appInfo.nativeLibraryDir);
+        Log.i("PermiSenseHook", "native lib: " + nativeLibPath);
+        XposedPermissionSettings.load(nativeLibPath);
         SensorHook sensorHook = new SensorHook(lpparam);
         sensorHook.hookSensors();
         sensorHooks.put(lpparam.packageName, sensorHook);
+    }
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        modulePath = startupParam.modulePath;
+    }
+
+    private static String mixLibPath(String modulePath, String nativeLibDir) {
+        if (modulePath == null || nativeLibDir == null) return null;
+        File lp = new File(nativeLibDir);
+        File lpp = lp.getParentFile();
+        if (lpp == null || !lpp.getName().equals("lib")) return null;
+        String arch = lp.getName();
+        File mp = new File(modulePath);
+        File mpp = mp.getParentFile();
+        if (mpp == null) return null;
+        return mpp.getAbsolutePath() + "/lib/" + arch + "/libpermisense.so";
+    }
+
+    public static String getNativeLibPath() {
+        return nativeLibPath;
     }
 }
